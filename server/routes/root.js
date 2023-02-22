5163d348ddec36d1112287df5145fb0fb806ba9a
eriley <me@eoghanriley.dev>
2023-02-21 21:32:56 -0500

    Added sending inputs to server

--- server/routes/root.js
+++ server/routes/root.js
@@ -3,7 +3,7 @@
 const queue = [];
 
 module.exports = async function(fastify, opts) {
-  fastify.get("/", async function(request, reply) {
+  fastify.get("/*", async function(request, reply) {
     if (request.headers.auth === process.env.AUTH) {
       if (queue.length > 0) {
         return { input: queue.shift() };
@@ -15,7 +15,29 @@ module.exports = async function(fastify, opts) {
     }
   });
 
-  fastify.post("/", async function(request, reply) {
+  fastify.post("/*", async function(request, reply) {
     queue.push(request.body.input);
+    reply
+      .code(200)
+      .header("Access-Control-Allow-Origin", "*")
+      .header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE")
+      .header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      )
+      .send({ status: "success" });
+  });
+
+  fastify.options("/*", async function(request, reply) {
+    reply
+      .code(204)
+      .header("Content-Length", "0")
+      .header("Access-Control-Allow-Origin", "*")
+      .header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE")
+      .header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      )
+      .send();
   });
 };
--- truffle-client/src/App.svelte
+++ truffle-client/src/App.svelte
@@ -6,11 +6,29 @@
   if (!document.referrer) {
     embed.hide();
   }
+
+  export let url = "http://127.0.0.1:3000/";
+
+  function post(key: string) {
+    console.log(JSON.stringify({ input: key }));
+    fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application.json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input: key }),
+      cache: "default",
+    });
+  }
 </script>
 
 <main class="bg-sky-500 h-full w-full rounded-xl border border-slate-900">
   <div class="dpad-1">
-    <button class="bg-stone-900 w-9 h-9 rounded-md my-6 mx-16 absolute" />
+    <button
+      class="bg-stone-900 w-9 h-9 rounded-md my-6 mx-16 absolute"
+      on:click={() => post("d1-up")}
+    />
     <button class="bg-stone-900 w-9 h-9 rounded-md my-24 mx-16 absolute" />
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-[3.75rem] mx-[6.5rem] absolute"
