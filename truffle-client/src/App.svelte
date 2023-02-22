02d98ca9ce66700a6c349cc883325de689da2fb7
eriley <me@eoghanriley.dev>
2023-02-22 09:18:39 -0500

    Added all inputs

--- truffle-client/src/App.svelte
+++ truffle-client/src/App.svelte
@@ -27,51 +27,76 @@
   <div class="dpad-1">
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-6 mx-16 absolute"
-      on:click={() => post("d1-up")}
+      on:click={() => post("d1_up")}
     />
-    <button class="bg-stone-900 w-9 h-9 rounded-md my-24 mx-16 absolute" />
+    <button
+      class="bg-stone-900 w-9 h-9 rounded-md my-24 mx-16 absolute"
+      on:click={() => post("d1_down")}
+    />
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-[3.75rem] mx-[6.5rem] absolute"
+      on:click={() => post("d1_right")}
     />
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-[3.75rem] mx-[1.45rem]  absolute"
+      on:click={() => post("d1_left")}
     />
   </div>
 
   <div class="button-set-1">
-    <button class="bg-stone-900 w-9 h-9 rounded-full my-6 mx-80 absolute" />
-    <button class="bg-stone-900 w-9 h-9 rounded-full my-24 mx-80 absolute" />
+    <button
+      class="bg-stone-900 w-9 h-9 rounded-full my-6 mx-80 absolute"
+      on:click={() => post("x1")}
+    />
+    <button
+      class="bg-stone-900 w-9 h-9 rounded-full my-24 mx-80 absolute"
+      on:click={() => post("b1")}
+    />
     <button
       class="bg-stone-900 w-9 h-9 rounded-full my-[3.75rem] mx-[22.5rem] absolute"
+      on:click={() => post("a1")}
     />
     <button
       class="bg-stone-900 w-9 h-9 rounded-full my-[3.75rem] mx-[17.45rem]  absolute"
+      on:click={() => post("y1")}
     />
   </div>
 
   <div class="dpad-2">
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-[13.5rem] mx-80 absolute"
+      on:click={() => post("d2_up")}
     />
-    <button class="bg-stone-900 w-9 h-9 rounded-md my-72 mx-80 absolute" />
+    <button
+      class="bg-stone-900 w-9 h-9 rounded-md my-72 mx-80 absolute"
+      on:click={() => post("d2_down")}
+    />
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-[15.75rem] mx-[22.5rem] absolute"
+      on:click={() => post("d2_right")}
     />
     <button
       class="bg-stone-900 w-9 h-9 rounded-md my-[15.75rem] mx-[17.45rem]  absolute"
+      on:click={() => post("d2_left")}
     />
   </div>
 
   <div class="button-set-2">
     <button
       class="bg-stone-900 w-9 h-9 rounded-full my-[13.5rem] mx-16 absolute"
+      on:click={() => post("x2")}
     />
-    <button class="bg-stone-900 w-9 h-9 rounded-full my-72 mx-16 absolute" />
+    <button
+      class="bg-stone-900 w-9 h-9 rounded-full my-72 mx-16 absolute"
+      on:click={() => post("b2")}
+    />
     <button
       class="bg-stone-900 w-9 h-9 rounded-full my-[15.75rem] mx-[6.5rem] absolute"
+      on:click={() => post("a2")}
     />
     <button
       class="bg-stone-900 w-9 h-9 rounded-full my-[15.75rem] mx-[1.45rem]  absolute"
+      on:click={() => post("y2")}
     />
   </div>
 </main>
